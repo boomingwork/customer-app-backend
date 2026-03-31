94352ff668dfd2407be00a9fc3fcaaafc12cfaea
boomingwork <boomingwork@gmail.com>
2026-03-31 19:45:49 +0000

    update frontend api with *

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ schema = strawberry.Schema(query=Query, mutation=Mutation)
 def get_context(db: Session = Depends(get_db)):
     return {"db": db}
 
-graphql_app = GraphQLRouter(schema, context_getter=get_context, allow_origins=["https://customer-app-frontend-5ajwvtv60-boomingwork-9487s-projects.vercel.app"])  # Allow all origins for GraphQL
+graphql_app = GraphQLRouter(schema, context_getter=get_context, allow_origins=["*"])  # Allow all origins for GraphQL
 
 app = FastAPI()
 
